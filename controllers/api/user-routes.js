2578e177a7eae6a2b92f1f1f77f1a784fd94bc95
vasilyl1 <likhovaido@gmail.com>
2023-02-26 14:58:14 -0500

    backend functionality for update, delete for posts and comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,15 +1,27 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // CREATE new post
 router.post('/post', withAuth, async (req, res) => {
   try {
-    const dbPostData = await Post.create(req.params.id, {
-      name: req.body.name,
-      body: req.body.body,
-      date: req.body.date,
-      user_id: req.session.user_id,
+    req.body.user_id = req.session.user_id;
+    const dbPostData = await Post.create(req.body);
+    const post = dbPostData.get({ plain: true });
+    res.render('post', { post, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE post
+router.put('/post/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
     });
     const post = dbPostData.get({ plain: true });
     res.render('post', { post, loggedIn: req.session.loggedIn });
@@ -19,18 +31,71 @@ router.post('/post', withAuth, async (req, res) => {
   }
 });
 
-// CREATE new comment with param referencing post id
-router.post('/comment/:postid', withAuth, async (req, res) => {
+// DELETE post
+router.delete('/post/:id', withAuth, async (req, res) => {
   try {
-    const dbCommentData = await Comment.create(req.params.id, {
-      body: req.body.body,
-      date: req.body.date,
-      post_id: req.param.postid,
-      user_id: req.session.user_id,
+    let dbPostData = await Post.destroy(req.body, {
+      where: {
+        id: req.params.id,
+      }
     });
+    dbPostData = await Post.findAll();
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// CREATE new comment with param referencing post id
+router.post('/comment/:id', withAuth, async (req, res) => {
+  try {
+    req.body.post_id = req.params.id;
+    req.body.user_id = req.session.user_id;
+    const dbCommentData = await Comment.create(req.body);
 
     const comment = dbCommentData.get({ plain: true });
     res.render('comment', { comment, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE comment
+router.put('/comment/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Comment.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    const comment = dbPostData.get({ plain: true });
+    res.render('comment', { comment, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// DELETE comment
+router.delete('/comment/:id', withAuth, async (req, res) => {
+  try {
+    const curPost = req.body.post_id;
+    let dbPostData = await Comment.destroy(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    dbPostData = await Post.findByPk(curPost, {
+      include: [
+        {model: Comment,},
+        {model: User},
+      ]
+    });
+    const post = dbPostData.get({ plain: true });
+    res.render('post', { post, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
