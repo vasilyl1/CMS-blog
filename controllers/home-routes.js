296c1ac1383b93232d4b761dc0f3c0588e703f69
vasilyl1 <likhovaido@gmail.com>
2023-02-25 20:30:21 -0500

    comments added for the post display

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,6 +16,7 @@ router.get('/', async (req, res) => {
     const posts = dbPostData.map((post) =>
       post.get({ plain: true })
     );
+    console.log(posts);
     res.render('homepage', {
       posts,
       loggedIn: req.session.loggedIn,
